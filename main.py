2a5d8f64dc30325c8fc951b517049f2a58f916bb
demo <61946705+kukkalu@users.noreply.github.com>
2023-08-28 16:45:17 +0530

    Update main.py

--- main.py
+++ main.py
@@ -285,7 +285,7 @@ async def account_login(bot: Client, m: Message):
                 if "pdf" in url:
                    cmd = f'yt-dlp -o "{name}.pdf" "{url1}"'
                 else:
-                    cmd = f'yt-dlp -o "{name}.mp4" --no-keep-video --no-check-certificate --remux-video mkv "{url1}"'
+                   cmd = f'yt-dlp -o "{name}.mp4" --no-keep-video --no-check-certificate --remux-video mkv "{url1}"'
                 try:
                    print("❤❤❤❤❤")
                    download_cmd = f"{cmd} -R 25 --fragment-retries 25 --external-downloader aria2c --downloader-args 'aria2c: -x 16 -j 32'"
@@ -295,39 +295,40 @@ async def account_login(bot: Client, m: Message):
                    print("💕💕")
 
                    if os.path.isfile(f"{name}.mkv"):
-                       filename = f"{name}.mkv"
+                      filename = f"{name}.mkv"
                    elif os.path.isfile(f"{name}.mp4"):
-                       filename = f"{name}.mp4"  
+                      filename = f"{name}.mp4"  
                    elif os.path.isfile(f"{name}.pdf"):
-                       filename = f"{name}.pdf"  
+                    filename = f"{name}.pdf"  
                    print("💕💕💕")
                    subprocess.run(f'ffmpeg -i "{filename}" -ss 00:01:00 -vframes 1 "{filename}.jpg"', shell=True)
                    await prog.delete (True)
                    reply = await m.reply_text(f"Uploading - ```{name}```")
                    try:
                        if thumb == "no":
-                          thumbnail = f"{filename}.jpg"
+                           thumbnail = f"{filename}.jpg"
                        else:
-                          thumbnail = thumb
-                    except Exception as e:
-                        await m.reply_text(str(e))
+                           thumbnail = thumb
+                   except Exception as e:
+                       await m.reply_text(str(e))
 
-                    dur = int(helper.duration(filename))
+                   dur = int(helper.duration(filename))
 
-                    start_time = time.time()
-                    if "pdf" in url1:
+                   start_time = time.time()
+                   if "pdf" in url1:
                        await m.reply_document(filename,caption=cc)
-                    else:
+                   else:
                        await m.reply_video(filename,supports_streaming=True,height=720,width=1280,caption=cc,duration=dur,thumb=thumbnail, progress=progress_bar,progress_args=(reply,start_time) )
-                    count+=1
-                    os.remove(filename)
+                   count+=1
+                   os.remove(filename)
 
-                    os.remove(f"{filename}.jpg")
-                    await reply.delete (True)
-                    time.sleep(1)
-                  except Exception as e:
+                   os.remove(f"{filename}.jpg")
+                   await reply.delete (True)
+                   time.sleep(1)
+                except Exception as e:
                     await m.reply_text(f"**downloading failed ❌**\n{str(e)}\n**Name** - {name}\n**Link** - `{url}` & `{url1}`")
-                  continue 
+                    continue 
+                
 
     except Exception as e:
         await m.reply_text(e)
