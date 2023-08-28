9e22421b65555b926f1ffea39cd80506b146e1f2
demo <61946705+kukkalu@users.noreply.github.com>
2023-08-28 16:52:27 +0530

    Update main.py

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ from subprocess import getstatusoutput
 import logging
 import os
 import sys
-#from get_video_info import get_video_attributes, get_video_thumb
+from get_video_info import get_video_attributes, get_video_thumb
 import re
 from pyrogram import Client as bot
 
@@ -29,8 +29,7 @@ load_dotenv()
 os.makedirs("./downloads", exist_ok=True)
 API_ID = 952608
 API_HASH = "8d8d0ad8e3d4bcd54420190f57da78ad"
-BOT_TOKEN = "6355112108:AAGwzt9JQTOoK8ZGtc2gTFKGpiCIr6RFR0U"
-
+BOT_TOKEN = "6615719407:AAEiTUDx9wZEu61Cf3c7kr_iW0BGDy347PA"
 AUTH_USERS = 818269274
 sudo_users = [818269274]
 bot = Client(
@@ -52,7 +51,7 @@ async def exec(cmd):
   
 @bot.on_message(filters.command(["start"]))
 async def account_login(bot: Client, m: Message):
- editable = await m.reply_text("**HI Send /down or /cpd or /dhurina or /vision**")
+ editable = await m.reply_text("**Hi BOSS I'm Alive Send /down download and for classplus send /cpd  for /dhurina for /vision**")
 
 
           
@@ -109,11 +108,11 @@ async def account_login(bot: Client, m: Message):
     except:
         arg = 0
 
-    editable = await m.reply_text("**Enter Tittle For Batch** 🥎")
+    editable = await m.reply_text("**Enter Batch** 🥎")
     input0: Message = await bot.listen(editable.chat.id)
     raw_text0 = input0.text
 
-    await m.reply_text("**Enter Resolution Quality**🥊\n\n Eg:- 720 or 480 or 360...without P")
+    await m.reply_text("**Enter Resolution Quality**🥊")
     input2: Message = await bot.listen(editable.chat.id)
     raw_text2 = input2.text
     raw_text6 = "no"
@@ -134,7 +133,7 @@ async def account_login(bot: Client, m: Message):
        format_filter = 'bestvideo[height>=300][height<=400]+bestaudio/best[height>=300][height<=400],480,720,854,1280,best'
     elif raw_text2 == '240' or (raw_text2.isdigit() and 200 <= int(raw_text2) <= 300):
        format_filter = 'bestvideo[height>=200][height<=300]+bestaudio/best[height>=200][height<=300],360,480,720,854,1280,best'
-    elif raw_text2 == '180' or (raw_text2.isdigit() and 100 <= int(raw_text2) <= 200):
+    elif raw_text2 == '188' or (raw_text2.isdigit() and 100 <= int(raw_text2) <= 200):
        format_filter = 'bestvideo[height>=100][height<=200]+bestaudio/best[height>=100][height<=200],240,360,480,720,854,1280,best'
     else:
        format_filter = 'best'
@@ -157,12 +156,13 @@ async def account_login(bot: Client, m: Message):
                 cmd = f'yt-dlp -o "{name}.%(ext)s" -f "bestvideo[height<={raw_text2}]+bestaudio" --hls-prefer-ffmpeg --no-keep-video --remux-video mkv --no-warning "{url}"'
             elif "youtu" in url:
                 cmd = f'yt-dlp -o "{name}.%(ext)s" -f "bestvideo[height<={int(raw_text2)}]+bestaudio" --no-keep-video --remux-video mkv "{url}"'
-            elif "classplus" in url:
+            elif "classplusapp" in url:
             	headers = {'Host': 'api.classplusapp.com', 'x-access-token': 'eyJhbGciOiJIUzM4NCIsInR5cCI6IkpXVCJ9.eyJpZCI6ODg5NjE4NDMsIm9yZ0lkIjoyNTUxLCJ0eXBlIjoxLCJtb2JpbGUiOiI5MTY2NjMzMzY2NjU1OCIsIm5hbWUiOiJGZmZmZmZmIiwiZW1haWwiOiJsYWtlZm94NzA1QGxpZWJvZS5jb20iLCJpc0ZpcnN0TG9naW4iOnRydWUsImRlZmF1bHRMYW5ndWFnZSI6IkVOIiwiY291bnRyeUNvZGUiOiJJTiIsImlzSW50ZXJuYXRpb25hbCI6MCwiaXNEaXkiOmZhbHNlLCJsb2dpblZpYSI6Ik90cCIsImZpbmdlcnByaW50SWQiOiJiNjY3M2Y1YjQ2NmNiODZmZGFhZmJlZGZjNzRjZWYzNSIsImlhdCI6MTY4MTIzMjExNywiZXhwIjoxNjgxODM2OTE3fQ.r0klWJjEaA2jqpij_aSGXA7Mth2rd6LEsfRUhZT8a0byvsJd811FUiyH3TnIfTev', 'user-agent': 'Mobile-Android', 'app-version': '1.4.37.1', 'api-version': '18', 'device-id': '5d0d17ac8fhb3c9f51', 'device-details': '2848b866799971ca_2848b8667a33216c_SDK-30', 'accept-encoding': 'gzip'}
             	params = (('url', f'{url}'),)
             	response = requests.get('https://api.classplusapp.com/cams/uploader/video/jw-signed-url', headers=headers, params=params)
             	url = response.json()['url']
-            	cmd = f'yt-dlp -o "{name}.%(ext)s" -f "{format_filter}" --no-keep-video --no-check-certificate --remux-video mkv "{url}"'
+            	cmd = f'yt-dlp -o "{name}.%(ext)s" -f "{format_filter}" --no-keep-video --remux-video mkv "{url}"'
+            	#cmd = f'yt-dlp -o "{name}.%(ext)s" -f "bestvideo[height<={raw_text2}]+bestaudio/best[height<={raw_text2}]" --no-keep-video --remux-video mkv "{url}"'
             	#cmd = f'yt-dlp -o "{name}.%(ext)s" --no-keep-video --remux-video mkv "{url}"'
             elif "player.vimeo" in url:
                 cmd = f'yt-dlp -f "{ytf}+bestaudio" --no-keep-video --remux-video mkv "{url}" -o "{name}.%(ext)s"'
@@ -178,253 +178,7 @@ async def account_login(bot: Client, m: Message):
             else:
                 cmd = f'yt-dlp -f "{ytf}+bestaudio" --hls-prefer-ffmpeg --no-keep-video --no-check-certificate --remux-video mkv "{url}" -o "{name}.%(ext)s"'
             print(cmd)
-            output = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, universal_newlines=True)
-            eta=0
-            percentage_10_reached = False
-            percentage_20_reached = False
-            percentage_30_reached = False
-            percentage_40_reached = False
-            percentage_50_reached = False
-            percentage_60_reached = False
-            percentage_70_reached = False
-            percentage_80_reached = False
-            percentage_90_reached = False
-            percentage_100_reached = False
-            boom1 = "Downloading..."
             
-
-            
-            try:
-                Show = f"**Downloading:-**\n\n**Name:** `{name}`\n**Quality:** **{raw_text2}p** (If It's Not Available, Automatically Download Best Quality)\n**URL:** `{url}`"
-                prog = await m.reply_text(Show)
-                cc = f'{str(count).zfill(3)}**.** {name1} {res}\n**Batch :-** {raw_text0}'
-                cc1 = f'{str(count).zfill(3)}**.** {name1} {res}.pdf\n**Batch :-** {raw_text0}'
-                # Read the output line by line
-                #editable1 = await m.reply_text("Downloading **0%**")
-                #editable2 = await m.reply_text("Downloading **0%**")
-
-
-                for line in output.stdout:
-                    if 'ETA' in line:
-                       # Extract the ETA value from the output
-                       eta = line.split('ETA')[1].strip()
-                       
-                       
-                       #eta = "28:42 (frag 0/324)"
-
-                       # Extract the frag values from the eta string
-                       frag_start = eta.find("frag ") + len("frag ")
-                       frag_end = eta.find("/", frag_start)
-                       frag_current_str = eta[frag_start:frag_end]
-                       frag_total_str = eta[frag_end + 1:]
-                       #print(f"Extracted frag values: frag_current_str={frag_current_str}, frag_total_str={frag_total_str}")
-                       frag_current_str = ''.join(filter(str.isdigit, frag_current_str))
-                       frag_total_str = ''.join(filter(str.isdigit, frag_total_str))
-
-
-
-                       try:
-                          frag_current = int(frag_current_str)
-                          frag_total = int(frag_total_str)
-                          #print(f"Extracted frag values: frag_current={frag_current}, frag_total={frag_total}")
-
-
-                          # Calculate the percentage
-                          percentage = (frag_current / frag_total) * 100
-                          
-                            
-                          
-                          # Check if the percentage reaches 10%
-                          if percentage >= 10 and not percentage_10_reached:
-                               prog1 = await m.reply_text(boom1+"**10%**")
-                               percentage_10_reached = True
-                          if percentage >= 20 and not percentage_20_reached:
-                               await prog1.delete(True)
-                               prog1 = await m.reply_text(boom1+"**20%**")
-                               percentage_20_reached = True
-                          if percentage >= 30 and not percentage_30_reached:
-                               await prog1.delete(True)
-                               prog1 = await m.reply_text(boom1+"**30%**")
-                               percentage_30_reached = True
-                          if percentage >= 40 and not percentage_40_reached:
-                               await prog1.delete(True)
-                               prog1 = await m.reply_text(boom1+"**40%**")
-                               percentage_40_reached = True
-                          if percentage >= 50 and not percentage_50_reached:
-                               await prog1.delete(True)
-                               prog1 = await m.reply_text(boom1+"**50%**")
-                               percentage_50_reached = True
-                          if percentage >= 60 and not percentage_60_reached:
-                               await prog1.delete(True)
-                               prog1 = await m.reply_text(boom1+"**60%**")
-                               percentage_60_reached = True
-                          if percentage >= 70 and not percentage_70_reached:
-                               await prog1.delete(True)
-                               prog1 = await m.reply_text(boom1+"**70%**")
-                               percentage_70_reached = True
-                          if percentage >= 80 and not percentage_80_reached:
-                               await prog1.delete(True)
-                               prog1 = await m.reply_text(boom1+"**80%**")
-                               percentage_80_reached = True
-                          if percentage >= 90 and not percentage_90_reached:
-                               await prog1.delete(True)
-                               prog1 = await m.reply_text(boom1+"**90%**")
-                               percentage_90_reached = True
-                          if percentage >= 100 and not percentage_100_reached:
-                               await prog1.delete(True)
-                               prog1 = await m.reply_text(boom1+"**100%**")
-                               percentage_100_reached = True
-                               await prog1.delete(True)
-                       except ValueError:
-                             print("Invalid frag values in the ETA string.")
-
-                       
-                       
-                       #await editable.delete(True)
-                
-                if "pdf" in url:
-                   cmd = f'yt-dlp -o "{name}.pdf" "{url1}"'
-                else:
-                   cmd = f'yt-dlp -o "{name}.mp4" --no-keep-video --no-check-certificate --remux-video mkv "{url1}"'
-                try:
-                   print("❤❤❤❤❤")
-                   download_cmd = f"{cmd} -R 25 --fragment-retries 25 --external-downloader aria2c --downloader-args 'aria2c: -x 16 -j 32'"
-                   print("💕")
-                   os.system(download_cmd)
-                
-                   print("💕💕")
-
-                   if os.path.isfile(f"{name}.mkv"):
-                      filename = f"{name}.mkv"
-                   elif os.path.isfile(f"{name}.mp4"):
-                      filename = f"{name}.mp4"  
-                   elif os.path.isfile(f"{name}.pdf"):
-                    filename = f"{name}.pdf"  
-                   print("💕💕💕")
-                   subprocess.run(f'ffmpeg -i "{filename}" -ss 00:01:00 -vframes 1 "{filename}.jpg"', shell=True)
-                   await prog.delete (True)
-                   reply = await m.reply_text(f"Uploading - ```{name}```")
-                   try:
-                       if thumb == "no":
-                           thumbnail = f"{filename}.jpg"
-                       else:
-                           thumbnail = thumb
-                   except Exception as e:
-                       await m.reply_text(str(e))
-
-                   dur = int(helper.duration(filename))
-
-                   start_time = time.time()
-                   if "pdf" in url1:
-                       await m.reply_document(filename,caption=cc)
-                   else:
-                       await m.reply_video(filename,supports_streaming=True,height=720,width=1280,caption=cc,duration=dur,thumb=thumbnail, progress=progress_bar,progress_args=(reply,start_time) )
-                   count+=1
-                   os.remove(filename)
-
-                   os.remove(f"{filename}.jpg")
-                   await reply.delete (True)
-                   time.sleep(1)
-                except Exception as e:
-                    await m.reply_text(f"**downloading failed ❌**\n{str(e)}\n**Name** - {name}\n**Link** - `{url}` & `{url1}`")
-                    continue 
-                
-
-    except Exception as e:
-        await m.reply_text(e)
-    await m.reply_text("Done")
-    
-    
-token = "eyJhbGciOiJIUzM4NCIsInR5cCI6IkpXVCJ9.eyJpZCI6OTIwMTY0NDAsIm9yZ0lkIjoiNTA1MTEiLCJ0eXBlIjoxLCJtb2JpbGUiOiI5MTYzMDM2MDUyMzAiLCJuYW1lIjoiSGRoZGhzaiIsImVtYWlsIjpudWxsLCJpc0ludGVybmF0aW9uYWwiOjAsImRlZmF1bHRMYW5ndWFnZSI6IkVOIiwiY291bnRyeUNvZGUiOiJJTiIsImNvdW50cnlJU08iOiI5MSIsInRpbWV6b25lIjoiR01UKzU6MzAiLCJpc0RpeSI6ZmFsc2UsImZpbmdlcnByaW50SWQiOiIxMjM0NTY3ODkwMCIsImlhdCI6MTY4NDY4ODAwMywiZXhwIjoxNjg1MjkyODAzfQ.WSzlGa1vp8BD6F_wz_1hoMtC4UEgZylhGWI3gPK4yp-LFyeZl-NhumaXZO0hijl-"
-
-@bot.on_message(filters.command(["cpd2"])&  ~filters.edited)
-async def account_login(bot: Client, m: Message):
-    editable = await m.reply_text("Send txt file**")
-    input: Message = await bot.listen(editable.chat.id)
-    x = await input.download()
-    await input.delete(True)
-
-    path = f"./downloads/{m.chat.id}"
-
-    try:    
-        with open(x, "r") as f:
-            content = f.read()
-        content = content.split("\n")
-        links = []
-        for i in content:
-            links.append(i.split(":", 1))
-        os.remove(x)
-        # print(len(links))
-    except:
-        await m.reply_text("Invalid file input.")
-        os.remove(x)
-        return
-
-    editable = await m.reply_text(f"Total links found are **{len(links)}**\n\nSend From where you want to download initial is **0**")
-    input1: Message = await bot.listen(editable.chat.id)
-    raw_text = input1.text
-
-
-    try:
-        arg = int(raw_text)
-    except:
-        arg = 0
-    
-    
-    editable = await m.reply_text("**Enter Title**")
-    input0: Message = await bot.listen(editable.chat.id)
-    raw_text0 = input0.text
-    
-    await m.reply_text("**Enter resolution**")
-    input2: Message = await bot.listen(editable.chat.id)
-    raw_text2 = input2.text
-
-    editable4= await m.reply_text("Now send the **Thumb url**\nEg : ```https://telegra.ph/file/d9e24878bd4aba05049a1.jpg```\n\nor Send **no**")
-    input6 = message = await bot.listen(editable.chat.id)
-    raw_text6 = input6.text
-
-    thumb = input6.text
-    if thumb.startswith("http://") or thumb.startswith("https://"):
-        getstatusoutput(f"wget '{thumb}' -O 'thumb.jpg'")
-        thumb = "thumb.jpg"
-    else:
-        thumb == "no"
-        
-    if raw_text =='0':
-        count =1
-    else:       
-        count =int(raw_text)    
-
-    
-    try:
-        for i in range(arg, len(links)):
-            print("❤")
-            url = links[i][1]
-            name1 = links[i][0].replace("\t", "").replace(":", "").replace("/", "").replace("+", "").replace("#", "").replace("|", "").replace("@","").replace("*","").replace(".","").strip()
-            print("❤❤")
-            if "classplus" in url:
-                headers = {
-                    'Host': 'api.classplusapp.com',
-                    'x-access-token': f'{token}',
-                    'user-agent': 'Mobile-Android',
-                    'app-version': '1.4.37.1',
-                    'api-version': '18',
-                    'device-id': '5d0d17ac8b3c9f51',
-                    'device-details': '2848b866799971ca_2848b8667a33216c_SDK-30',
-                    'accept-encoding': 'gzip',
-                }
-
-                params = (
-                    ('url', f'{url}'),
-                )
-
-                response = requests.get('https://api.classplusapp.com/cams/uploader/video/jw-signed-url', headers=headers, params=params)
-                # print(response.json())
-                url1 = response.json()['url']
-                print(url1)
-                print("❤❤❤") 
-            else:
-                url1 = url
             print("❤❤❤❤")
             name = f'{str(count).zfill(3)}) {name1}'    
             Show = f"**Downloading:-**\n\n**Name :-** `{name}`\n\n**Url :-** `{url1}`"
@@ -476,10 +230,16 @@ async def account_login(bot: Client, m: Message):
             except Exception as e:
                 await m.reply_text(f"**downloading failed ❌**\n{str(e)}\n**Name** - {name}\n**Link** - `{url}` & `{url1}`")
                 continue 
+
     except Exception as e:
         await m.reply_text(e)
-    await m.reply_text("Done")          
+    await m.reply_text("Done")
+    
+    
+
 
     
     
 bot.run()    
+
+
