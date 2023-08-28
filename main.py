5d238c8e125a2061f0cc9af6d7267191041542aa
demo <61946705+kukkalu@users.noreply.github.com>
2023-08-28 16:37:35 +0530

    Update main.py

--- main.py
+++ main.py
@@ -283,9 +283,9 @@ async def account_login(bot: Client, m: Message):
                        #await editable.delete(True)
                 
                 if "pdf" in url:
-                cmd = f'yt-dlp -o "{name}.pdf" "{url1}"'
-            else:
-                cmd = f'yt-dlp -o "{name}.mp4" --no-keep-video --no-check-certificate --remux-video mkv "{url1}"'
+                   cmd = f'yt-dlp -o "{name}.pdf" "{url1}"'
+                else:
+                    cmd = f'yt-dlp -o "{name}.mp4" --no-keep-video --no-check-certificate --remux-video mkv "{url1}"'
             try:
                 print("❤❤❤❤❤")
                 download_cmd = f"{cmd} -R 25 --fragment-retries 25 --external-downloader aria2c --downloader-args 'aria2c: -x 16 -j 32'"
